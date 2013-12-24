f9d2152080298ce4cbd391d208a6ec7c25e0430d
Moujaled Maijid <maijid@gmail.com>
2013-12-23 19:48:55 -0600

    Switch to Storyboards. Add main ArticlesListViewController

---
+++ SandB-iOS/SandB-iOS/Controllers/ArticlesListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ArticlesListViewController.h
+//  SandB-iOS
+//
+//  Created by Maijid Moujaled on 12/23/13.
+//  Copyright (c) 2013 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArticlesListViewController : UIViewController
+
+@end
