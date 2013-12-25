19ea81e88dacb3832381615f7cee411b3de0c40a
Moujaled Maijid <maijid@gmail.com>
2013-12-24 20:05:54 -0600

    Create SandBClient for all networking requests.

---
+++ SandB-iOS/SandB-iOS/Model/SandBClient.h
@@ -0,0 +1,13 @@
+//
+//  SandBClient.h
+//  SandB-iOS
+//
+//  Created by Maijid Moujaled on 12/24/13.
+//  Copyright (c) 2013 Grinnell AppDev. All rights reserved.
+//
+
+#import "AFHTTPSessionManager.h"
+
+@interface SandBClient : AFHTTPSessionManager
+
+@end
---
+++ SandB-iOS/SandB-iOS/Model/SandBClient.m
@@ -0,0 +1,13 @@
+//
+//  SandBClient.m
+//  SandB-iOS
+//
+//  Created by Maijid Moujaled on 12/24/13.
+//  Copyright (c) 2013 Grinnell AppDev. All rights reserved.
+//
+
+#import "SandBClient.h"
+
+@implementation SandBClient
+
+@end
